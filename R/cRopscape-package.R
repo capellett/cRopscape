453e750097130d29276f87a8daa9fac465aeb33c
alex <c.alex.pellett@gmail.com>
2018-12-20 21:19:41 -0500

    doc

---
+++ R/cRopscape-package.R
@@ -0,0 +1,9 @@
+#' United States Department of Agriculture Cropland Data Layer
+#'
+#' Data from the United States Department of Agriculture Cropland Data Layer (CDL),
+#'     also known as "Cropscape".
+#'
+#' @docType package
+#' @name cRopscape
+#'
+NULL
